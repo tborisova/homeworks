c758496a8287cd3ae810f14aa614f3170d042962
Tsvetelina Borisova <ts.borisova3@gmail.com>
2014-11-13 17:54:00 +0200

    Add homework 2

--- nosql-bases-mongo/homework2/homework2.rb
+++ nosql-bases-mongo/homework2/homework2.rb
@@ -1,7 +1,3 @@
-# encoding: UTF-8
-# coding: UTF-8
-# -*- coding: UTF-8 -*-
-
 require 'mongo'
 require 'bson'
 
@@ -10,27 +6,17 @@ db     = client['homework1']
 jokes = db['jokes']
 users = db['users']
 
-jokes_to_insert = [ { author: "Господин Бъзик", text: "Бил Гейтс казал на жена си:
-                    ­  Вчера ходих до банката за заем.
-                    ­  Чакай, за какво ти е на теб заем?
-                    ­  Не на мен. На банката…", rating: 6, characters: ["Бил Гейтс"] }, 
-                    { author: "Дъмбълдор", text: "Хари се връща от Забранената гора и казва на Рон: Събрах 
-                      гъбки за професор Снейп. Рон: А ако са отровни? Хари: Какво значи 'ако'?", rating: 10,
-                      characters: ["Хари Потър", "Снейп", "Рон Уизли"] },
-                    { author: "Студент", text: "Двама студенти си говорят: Как мислиш, математическият анализ наука,
-                      ли е? Не е, ако беше наука щяха най-напред да го опитват върху животни.", rating: 8,
-                      characters: ["студенти"] },
-                    { author: "Господин Бъзик", text: "Една учителка казала на Иванчо:  Ти защо си написал \"къшта\"?
-                      Я излез на дъската и напиши думата двайсет пъти както трябва, за да я запомниш за цял живот!
-                      След минутка учителката се обърнала и видяла на черната дъска:
-                      @Repeat(\"къща\";20)", rating: 9, characters: ["Иванчо", "учителката"] },
-                    { author: "Дъмбълдор", text: "Рон влиза в магазина Трите Метли и дава банкнота от Монополи и казва: 
-                      Един плюшен Виктор Крум.   Момче, това не са истински пари!   То и Крум не е истински, ама някой казва ли
-                      ви го?", rating: 7, characters: ["Виктор Крум", "Рон Уизли"] } ]
+jokes_to_insert = [ { author: "User1", text: "This is joke 1", rating: 6, characters: ["Bill Gates"] }, 
+                    { author: "Dumbledore", text: "This is joke 2", rating: 10,
+                      characters: ["Harry Potter", "Snape", "Ron Weasley"] },
+                    { author: "Student", text: "This is joke 3", rating: 8,
+                      characters: ["students"] },
+                    { author: "User1", text: "This is joke 4", rating: 9, characters: ["Ivancho", "teacher"] },
+                    { author: "Dumbledore", text: "This is joke 5", rating: 7, characters: ["Victor", "Ron Weasley"] } ]
 
-users_to_insert = [ { name: "Господин Бъзик", email: "joke_master@abv.bg", likes: [ "Бил Гейтс", "Чък Норис", "Чапай", "Петка" ] },
-                    { name: "Дъмбълдор", email: "dubmbledore@hogwarts.com", likes: [ "Severus Snape", "James Potter" ] },
-                    { name: "Студент", email: "example3@abv.bg", likes: [ "Хари Потър", "Чък Норис" ] } ]
+users_to_insert = [ { name: "User1", email: "joke_master@abv.bg", likes: [ "Bill Gates", "Chuck Noris", "Chapai", "Petka" ] },
+                    { name: "Dumbledore", email: "dubmbledore@hogwarts.com", likes: [ "Severus Snape", "James Potter" ] },
+                    { name: "Student", email: "example3@abv.bg", likes: [ "Harry Potter", "Chuck Noris" ] } ]
 
 jokes_to_insert.each do |joke|
   jokes.insert(joke)
@@ -40,27 +26,33 @@ users_to_insert.each do |user|
   users.insert(user)
 end
 
-updated_jokes = db['jokes'].update({ author: 'Дъмбълдор'}, {'$set' => {"black_humor" => true}}).to_a
+db['jokes'].update({ author: 'Dumbledore'}, {:$set => {"black_humor" => true}}, {multi: true, upsert: false})
 
-updated_jokes.each do |joke|
-  p joke
+p 'will show all jokes after update:'
+db['jokes'].find({ author: 'Dumbledore' }) do |cursor|
+  cursor.each do |joke|
+    p joke
+  end
 end
 
-sorted_jokes = db['jokes'].find({ :characters => { '$in' => ['Бил Гейтс', 'Рон Уизли'] } }).sort({rating: 1}).to_a
+p 'will show sorted jokes by rating: 1'
+sorted_jokes = db['jokes'].find({ :characters => { '$in' => ['Bill Gates', 'Ron Weasley'] } }).sort({rating: -1}).to_a
 
 sorted_jokes.each do |joke|
   p joke
 end
 
-p db['jokes'].find().count
+
+# p 'All jokes count:'
+# p db['jokes'].find().count
 
 db['jokes'].remove({ :rating => { '$lte' => 7 } })
 
+p 'Jokes after removing rating: lte => 7:'
 p db['jokes'].find().count
 
-count = db['jokes'].find({ author: 'Господин Бъзик' }).count
-
-p count
+p 'Jokes with author User1:'
+p db['jokes'].find({ author: 'User1' }).count
 
 jokes.drop
 users.drop
