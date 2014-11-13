3af4e082c533aa223e0a094a375c82dde8610995
Tsvetelina Borisova <ts.borisova3@gmail.com>
2014-11-13 13:12:33 +0200

    Add task2 homework 6 for fp2014

--- homeworks-fp2014/homework6/lab6.scm
+++ homeworks-fp2014/homework6/lab6.scm
@@ -109,15 +109,29 @@
     ((null? matrix) '())
     ((cons (append (car matrix) (without (car matrix) (- (length (car matrix)) 1))) (duplicate (cdr matrix))))))
 
-;(define (determinant matrix)
- ; (define positive-diagonal (diagonal matrix))
-  ;(define negative-diagonal ())
-;)
+(define positive-el '(((1 1) (2 2) (3 3)) ((1 2) (2 3) (3 1)) ((1 3) (2 1) (3 2))))
+(define negative-el '(((3 1) (2 2) (1 3)) ((3 2) (2 3) (1 1)) ((3 3) (2 1) (1 2))))
 
-;(1,1)(2,2)(3,3)+ (1,2)(2,3)(3, 1) +(1,3)(2,1)(3, 2)  - (3,1)(2,2)(1,3) - (3,2)(2,3)(1,1) - (3, 3),(2, 1)(1, 2)
+(define (determinant matrix)
+  (- (sum-of-elements positive-el matrix) (sum-of-elements negative-el matrix))
+)
 
-(define (next-sarrus coords)
+(define (sum-of-elements el-coords matrix)
+  (sum-of-elements-helper el-coords matrix 0)
+)
+
+(define (sum-of-elements-helper el-coords matrix res)
   (cond
-   ((car coords) == )  
-  )  
+    ((null? el-coords) res)
+    (else (sum-of-elements-helper (cdr el-coords) matrix (+ res (proizvedenie (car el-coords) matrix 1))))))
+
+(define (proizvedenie list-of-coords matrix res)
+  (cond
+    ((null? list-of-coords) res)
+    (else (proizvedenie (cdr list-of-coords) matrix (* (el-at-row-col matrix (- (nth-element (car list-of-coords) 0) 1) (- (nth-element (car list-of-coords) 1) 1)) res)))
+  )
+)
+
+(define (el-at-row-col matrix row col)
+  (nth-element (nth-element matrix row) col)
 )
