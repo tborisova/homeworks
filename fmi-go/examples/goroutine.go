b810796c0c440a301c5df509b99930d6495b9a78
Tsvetelina Borisova <ts.borisova3@gmail.com>
2014-11-15 23:04:49 +0200

    Add goroutines example

--- fmi-go/examples/goroutine.go
+++ fmi-go/examples/goroutine.go
@@ -1,38 +1,28 @@
-// A _goroutine_ is a lightweight thread of execution.
-
 package main
+import(
+  "fmt"
+  "math/rand"
+  "time"
+)
+// func f(n int) {
+//   for i := 0; i < 10; i++ {
+//     fmt.Println(n, ":", i)
+//   }
+// }
 
-import "fmt"
-
-func f(from string) {
-    for i := 0; i < 3; i++ {
-        fmt.Println(from, ":", i)
-    }
+func f(n int) {
+  for i := 0; i < 10; i++ {
+    fmt.Println(n, ":", i)
+    amt := time.Duration(rand.Intn(250))
+  time.Sleep(time.Millisecond * amt)
+  }
 }
 
 func main() {
+  for i := 0; i < 10; i++ {
+    go f(i)
+  }
 
-    // Suppose we have a function call `f(s)`. Here's how
-    // we'd call that in the usual way, running it
-    // synchronously.
-//    f("direct")
-
-    // To invoke this function in a goroutine, use
-    // `go f(s)`. This new goroutine will execute
-    // concurrently with the calling one.
-    go f("goroutine")
-
-    // You can also start a goroutine for an anonymous
-    // function call.
-    go func(msg string) {
-        fmt.Println(msg)
-    }("going")
-
-    // Our two goroutines are running asynchronously in
-    // separate goroutines now, so execution falls through
-    // to here. This `Scanln` code requires we press a key
-    // before the program exits.
-    var input string
-    fmt.Scanln(&input)
-    fmt.Println("done")
+  var input string
+  fmt.Scanln(&input)
 }
