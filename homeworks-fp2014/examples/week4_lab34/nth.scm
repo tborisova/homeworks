c9a53323500af80c10add01dca275538988d2198
Tsvetelina Borisova <ts.borisova3@gmail.com>
2014-11-20 15:14:00 +0200

    Add solution to 'split' function

--- homeworks-fp2014/examples/week4_lab34/nth.scm
+++ homeworks-fp2014/examples/week4_lab34/nth.scm
@@ -19,3 +19,28 @@
 
 (join " " (list "Scheme" "is" "an" "akward" "language"))
 (join "1!" (list "One" "Two" "Three"))
+
+(define (split delimiter str)
+  (split-helper (car (string->list delimiter)) (string->list str) '() '())
+)
+
+(define (split-helper delimiter list-of-chars new-list current-list)
+  (cond
+    ((null? list-of-chars) (append new-list (list (list->string current-list))))
+    ((equal? delimiter (car list-of-chars)) (split-helper delimiter (cdr list-of-chars) (append new-list (list (list->string current-list))) '()))
+    (else (split-helper delimiter (cdr list-of-chars) new-list (append current-list (list (car list-of-chars)))))
+  )
+)
+
+(split " " "Scheme is an akward language")
+
+
+
+
+
+
+
+
+
+
+
