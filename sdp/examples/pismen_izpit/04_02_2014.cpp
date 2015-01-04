835e948ff42e4ccf9b3ae5e239f5935b6580d194
Tsvetelina Borisova <ts.borisova3@gmail.com>
2015-01-04 19:33:23 +0200

    Add pismen_izpit folder + solutions in sdp

---
+++ sdp/examples/pismen_izpit/04_02_2014.cpp
@@ -0,0 +1,177 @@
+
+// Задача 2. Дадена е опашка от цели числа q. За нея са дефинирани функциите:
+
+// s(x) = (x + x) % 10, ако x принадлежи на q и е равен на сбора на останалите елементи на опашката. 
+///В противен случай s(x) = x.
+
+// m(x) = (x * x) % 10, ако x принадлежи на q и е равен на произведението на останалите елементи на опашката. 
+//В противен случай m(x) = x.
+
+// Нека освен това е даден под формата на низ израз от вида
+// <израз> := <цифра> | 
+//                               s(<израз>) | m(<израз>) 
+// Да се реализира външна функция, която пресмята стойността на израза.
+
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+#include "stack_connected.cpp"
+#include "queue_linked.cpp"
+#include "cyclicList.cpp"
+#include "linked_list_two_links.cpp"
+
+int some_func(queue<int>& q,const char* s){
+  int n = strlen(s);
+  int i= 0;
+  stack<char> stack_for_operations;
+  stack<int> stack_for_operands;
+  while(i < n){
+    if(s[i] == 'm' || s[i] == 's'){
+      cout << "will add " << s[i] << endl;
+      stack_for_operations.push(s[i]);
+    }else if(s[i] >= '0' && s[i] <= '9'){
+      int num = s[i] - '0';
+      cout << "will add " << num << endl;
+      stack_for_operands.push(num);
+    }else if(s[i] == ')'){
+      int num;
+      char ch;
+      stack_for_operations.pop(ch);
+      stack_for_operands.pop(num);
+      cout << "pop " << ch << "  " << num << endl;
+      switch(ch){
+        case 's': stack_for_operands.push(q.s(num)); break;
+        case 'm': stack_for_operands.push(q.m(num)); break;
+      }
+    }
+
+    i++;
+  }
+
+  int result;
+  stack_for_operands.pop(result);
+
+  return result;
+}
+
+bool stange_func(DLList< cyclicList<char> >& list, const char **wordsArray, int size){
+  bool matchedWords[size];
+  for(int i = 0; i < size; i++){
+    matchedWords[i] = false;
+  }
+  
+  list.iterStart();
+  elem_link2< cyclicList<char> >* el = list.iterNext();
+  
+  while(el){
+    //el is one cyclic list el->inf is the cyclic list
+    for(int i = 0; i < size; i++){
+      el->inf.iterStart()
+      elem_cyclic<char>* el_cyc = el->inf.iter();
+      int lenght_of_word = strlen(wordsArray[i]);
+
+      if(matchedWords[i] == false){
+        for(int j = 0; j < lenght_of_word; i++){
+          int n = 0;
+          do{
+            n++;
+            el_cyc = el->inf.iter();
+          }while(el_cyc->inf == wordsArray[i][j]);
+
+          if(el_cyc->inf == wordsArray[i][j]){
+            el_cyc = el->inf.iter();
+            j++
+            while(el_cyc && el_cyc->inf == wordsArray[i][j] && j < lenght_of_word){
+              j++;
+              el_cyc = el->inf.iter();
+            }
+          }
+
+          if(j == lenght_of_word) matchedWords[i] = true;
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
+int main(){
+  //zad 2
+  // queue<int> q;
+
+  // for(int i = 0; i < 10; i++){
+  //   q.push(i);
+  // }
+  // q.push(55);
+  // const char *s  = "s(m(s(3)))";
+  // int res = some_func(q, s);
+
+  // cout << res << endl;
+  // return 0;
+
+  //zad 3
+
+  DLList< cyclicList<char> > list;
+
+  cyclicList<char> clist;
+
+  clist.toEnd('a');
+  clist.toEnd('n');
+  clist.toEnd('e');
+  clist.toEnd('l');
+  clist.toEnd('y');
+
+  list.toEnd(clist);
+
+  cyclicList<char> clist2;
+
+  clist2.toEnd('e');
+  clist2.toEnd('l');
+  clist2.toEnd('e');
+  clist2.toEnd('n');
+  clist2.toEnd('i');
+
+  list.toEnd(clist2);
+
+  cyclicList<char> clist3;
+
+  clist3.toEnd('p');
+  clist3.toEnd('e');
+  clist3.toEnd('t');
+  clist3.toEnd('r');
+  clist3.toEnd('i');
+  clist3.toEnd('n');
+  clist3.toEnd('a');
+
+  list.toEnd(clist3);
+  list.iterStart();
+
+  elem_link2< cyclicList<char> > *el = list.iterNext();
+
+  while(el){
+    el->inf.print();
+    el = list.iterNext();
+  }
+
+  const char *wordsArray[] = {"Asd", "asdas"};
+
+  stange_func(list, wordsArray, 2);
+  return 0;
+}
+
+/*
+Задача 3.  Даден е двойносвързан списък DL от циклични списъци от символи от “a” до “z”. 
+Да се напише външна функция, която по даден масив от думи проверява дали думите в него могат да се “прочетат” 
+последователно от цикличните списъци в рамките на двойносвързания списък. Ако при успешно преминаване на списъка 
+DL останат думи от масива, които не се проверени, то елементите на DL трябва да се проверят и в обратна посока за 
+останалата част от изречението.
+Забележка.
+Една дума може да се прочете от цикличен списък, ако първата буква на думата се съдържа в 
+цикличния списък и останалите букви могат да се прочетат като списъкът се итерира (при нужда и циклично).
+ От своя страна двойносвързаният списък може да се обходи най-много веднъж в двете посоки.
+Пример. Ако DL съдържа цикличните списъци от символи: a n e l y; e l e n i; p e t r i n a, 
+а масивът съдържа низовете: "nely", "elen", "petri", "napet", "eleni", "lya" резултатът от изпълнението на функцията е true.
+*/
---
+++ sdp/examples/pismen_izpit/stack_connected.cpp
@@ -0,0 +1,181 @@
+// #include <cstdlib>
+// #include <cassert>
+// #include <iostream>
+// using namespace std;
+
+template <typename T>
+struct elem{
+  T inf;
+  elem *link;
+};
+
+template <typename T=int>
+class stack{
+public:
+  stack();
+  ~stack();
+  stack(const stack&);
+  stack& operator=(const stack&);
+  bool empty() const;
+  void push(const T&);
+  void pop(T&);
+  void top(T&) const;
+  void print();
+  int length();
+private:
+  elem<T> *start;
+  void copyStack(const stack&);
+  void deleteStack();
+};
+
+template<typename T>stack<T>::stack(){
+  start = NULL;
+}
+
+template<typename T>stack<T>::~stack(){
+  deleteStack();
+}
+
+template<typename T>stack<T>::stack(const stack& st){
+  copyStack(st);
+}
+
+template<typename T> stack<T>& stack<T>::operator=(const stack<T>& st){
+  if (this != &st){ 
+    deleteStack();
+    copyStack(st);
+  }
+  return *this;
+}
+
+template<typename T> void stack<T>::copyStack(const stack<T>& st){
+  if(st.start){
+    elem<T> *p = st.start, *q = NULL, *s = NULL;
+    start = new elem<T>;
+    assert(start != NULL);
+    start->inf = p->inf;
+    start->link = q;
+    q = start;
+    p = p->link;
+    while(p){
+      s = new elem<T>;
+      assert(s != NULL);
+      s->inf = p->inf;
+      q->link = s;
+      q = s;
+      p = p->link;
+    }
+    q->link = NULL;
+  }
+  else start = NULL;
+}
+
+template<typename T>
+void stack<T>::deleteStack(){
+  elem<T> *p;
+  while(start){
+    p = start;
+    start = start->link;
+    delete p;
+  }
+}
+
+template<typename T> bool stack<T>::empty() const{
+  return start == NULL;
+}
+
+template <typename T> void stack<T>::push(const T& s){
+  elem<T>* p = start;
+  start = new elem<T>;
+  assert(start != NULL);
+  start->inf = s;
+  start->link = p;
+}
+
+template <typename T> void stack<T>::pop(T& s){
+  if(start){
+    s = start->inf;
+    elem<T>* p = start;
+    start = start->link;
+    delete p;
+  }else{
+    cout << "stack is empty\n";
+    exit(1);
+  }
+}
+
+template <typename T> void stack<T>::top(T& s) const{
+  if(start){
+    s = start->inf;
+  }else{
+    cout << "stack is empty\n";
+    exit(1);
+  }
+}
+
+template<typename T> void stack<T>::print(){
+  T x;
+
+  while(!empty()){
+    pop(x);
+    cout << x << " ";
+  }
+  cout << endl;
+}
+
+template <typename T> int stack<T>::length(){
+  int n = 0;
+  elem<T>* p = start;
+
+  while(p){
+    n++;
+    p = p->link;
+  }
+
+  return n;
+}
+
+
+template <typename T> void accumulate(T (*op)(T, T), T& null_value, stack<T>& st){
+  T x;
+
+  while(!st.empty()){
+    st.pop(x);
+    null_value = op(null_value, x);
+  }
+
+  return;
+}
+
+int my_int_func(int x, int y){
+  return x*y;
+}
+
+int my_map_func(int x){
+  return x+2;
+}
+
+template <typename T> void map(T (*func)(T), stack<T>& st, stack<T>& res){
+  T x;
+
+  while(!st.empty()){
+    st.pop(x);
+    res.push(func(x));
+  }
+}
+
+// int main(){
+//   stack<int> st, st2;
+//   st.push(10);
+//   st.push(20);
+//   st.push(30);
+
+//   map(my_map_func, st, st2);
+  
+//   // int x = 0;
+//   // accumulate(my_int_func, x, st2);
+
+//   st2.print(); //it empties the stack
+//   // cout << x << endl;
+//   return 0;
+// }
