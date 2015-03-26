1bf93fd608ebc9a37202411a7e1808316b9b92f5
Tsvetelina Borisova <ts.borisova3@gmail.com>
2015-03-26 19:41:58 +0200

    new version

--- 4th-semester/daa-praktikum/grafik.cpp
+++ 4th-semester/daa-praktikum/grafik.cpp
@@ -4,39 +4,58 @@
 #include <map>
 using namespace std;
 
+struct event{
+	long start;
+	long duration;
+
+	bool operator<(event& other){
+		return start < other.start;
+	}
+
+	bool operator<(event& other) const{
+		return start < other.start;
+	}
+
+	bool operator<(const event& other) const{
+		return start < other.start;
+	}
+
+};
+
 int main(){
 	int n = 0;
 	long start_event, end_event;
 	int i = 0;
-	map<long, long> events;
+	event events[10000];
 
 	while(scanf("%ld %ld", &start_event, &end_event) == 2){
-		events[start_event] = end_event + start_event;
+		events[i].start = start_event;
+		events[i].duration = start_event + end_event;
+		i++;
 	}
-	map<long, long>::iterator it, founded, it2;
+
+	n = i;
 	int max = 1;
-	int old_max = 1;
+	event *pointer;
+	int old_max = 0;
+	sort(events, events + n);
 
-	for(it=events.begin(); it!=events.end();it++){
-		
-		for(it2 = it; it2 != events.end();)	{
-
-			founded = events.find(it2->second);
-
-			if(founded != events.end()){
-				it2 = founded;
+	for(i = 0; i < n/2; i++){
+		for(int j = i; j < n;){
+		    pointer = lower_bound(events, events + n, events[j]);
+			if(*pointer.start == events[j].duration){
 				max++;
+				j = pointer - events;
 			}else{
-				it2++;
-			}	
+				j++;
+			}
 		}
 
 		if(old_max < max){
 			old_max = max;
 		}
-
 		max = 1;
 	}
-	
+
 	printf("%d\n", old_max);
 }
