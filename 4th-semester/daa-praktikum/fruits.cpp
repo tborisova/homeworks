4bdb9955a993dd47d6896a2fb1848f1f1b74aec2
Tsvetelina Borisova <ts.borisova3@gmail.com>
2015-05-14 17:38:19 +0300

    Add more solutions

--- 4th-semester/daa-praktikum/fruits.cpp
+++ 4th-semester/daa-praktikum/fruits.cpp
@@ -3,11 +3,12 @@
 #include <map>
 using namespace std;
 
-#define N 1000000
+#define N 10000
 
 int main(){
   long n, m;
   long upper_bound = 0, count, fruit;
+  long fisrt_fruit = 1;
   long lower_bounds[N];
   long upper_bounds[N];
 
@@ -16,20 +17,22 @@ int main(){
   for(int i = 0; i < n; i++){
     cin >> fruit;
     upper_bound += fruit;
-    lower_bounds[i] = fruit;
+    lower_bounds[i] = fisrt_fruit;
     upper_bounds[i] = upper_bound;
+    fisrt_fruit = upper_bound + 1;
   }
 
   
   cin >> m;
-  
+  // cout << "here" << endl;
   while(m){
     cin >> fruit;
     long i = 0;
     long j = n-1;
 
     while(1){
-      if((lower_bounds[i] >= fruit && upper_bounds[j] <= fruit) ||  lower_bounds[i] == fruit || upper_bounds[j] == fruit){
+      cout << " " << fruit << " " << lower_bounds[i] << " " << upper_bounds[j] << endl;
+      if((lower_bounds[i] >= fruit && fruit <= upper_bounds[j]) ||  lower_bounds[i] == fruit || upper_bounds[j] == fruit){
         cout << i + 1 << endl;
         break;
       }else if(upper_bounds[j] > fruit){
